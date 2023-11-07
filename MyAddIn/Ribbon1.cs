e14f509b34bacba2cdb8e51898d8c39c90ac2993
oliverlinux <105554817+oliverlinux@users.noreply.github.com>
2023-11-07 09:35:28 -0500

    Upload the project

---
+++ MyAddIn/Ribbon1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyAddIn
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+        
+            string usertext = "";
+            try
+            {
+                usertext = File.ReadAllText(System.Environment.SpecialFolder.LocalApplicationData + "data.dat");
+                
+            }
+            catch
+            {
+                Form TheSettingsForError = new Settings();
+                TheSettingsForError.Visible = true;
+                TheSettingsForError.Activate();
+                
+            }
+            if (usertext != "")
+            {
+                if (Globals.Ribbons.Ribbon1.checkBox1.Checked == true)
+                {
+                    Globals.ThisAddIn.Application.ActiveDocument.Content.Text = Globals.ThisAddIn.Application.ActiveDocument.Content.Text + "\n" + usertext;
+                }
+                else
+                {
+                    Globals.ThisAddIn.Application.ActiveDocument.Content.InsertAfter(usertext);
+                }
+            }
+            
+            
+        }
+        
+
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            Form TheSettings = new Settings();
+            TheSettings.Visible = true;
+            TheSettings.Activate();
+            
+        }
+
+    }
+}
---
+++ MyAddIn/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyAddIn
+{
+    public partial class Settings : Form
+
+    {
+        
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            File.WriteAllText(System.Environment.SpecialFolder.LocalApplicationData + "data.dat",richTextBox1.Text);
+
+        }
+    }
+}
